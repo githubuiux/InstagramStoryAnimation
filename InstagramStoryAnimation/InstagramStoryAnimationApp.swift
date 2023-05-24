7fc73902fbaae50d903d5a8f84fb6cecc08c0d2c
githubuiux <uiuxista@gmail.com>
2023-05-24 10:35:11 +0300

    Initial Commit

---
+++ InstagramStoryAnimation/InstagramStoryAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramStoryAnimationApp.swift
+//  InstagramStoryAnimation
+//
+//  Created by Levent on 24.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramStoryAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
